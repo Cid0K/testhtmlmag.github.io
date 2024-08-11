7d49e9f12024d5a36c9b96a3cee94a38f9e763d7
Cid0K <cidkonage@gmail.com>
2024-08-11 18:49:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+let productCounts = {};
 let tg = window.Telegram.WebApp;
 
 tg.expand();
@@ -5,92 +6,84 @@ tg.expand();
 tg.MainButton.textColor = '#FFFFFF';
 tg.MainButton.color = '#2cab37';
 
-let item = "";
+function updateCount(productId, decrement = false) {
+  const quantityElement = document.getElementById(`quantity${productId}`);
+  let count = productCounts[productId] || 0;
 
-let btn1 = document.getElementById("btn1");
-let btn2 = document.getElementById("btn2");
-let btn3 = document.getElementById("btn3");
-let btn4 = document.getElementById("btn4");
-let btn5 = document.getElementById("btn5");
-let btn6 = document.getElementById("btn6");
+  if (decrement && count > 0) {
+    count--;
+  } else {
+    count++;
+  }
 
-btn1.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 1!");
-		item = "1";
-		tg.MainButton.show();
-	}
-});
-
-btn2.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 2!");
-		item = "2";
-		tg.MainButton.show();
-	}
-});
-
-btn3.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 3!");
-		item = "3";
-		tg.MainButton.show();
-	}
-});
-
-btn4.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 4!");
-		item = "4";
-		tg.MainButton.show();
-	}
-});
-
-btn5.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 5!");
-		item = "5";
-		tg.MainButton.show();
-	}
-});
-
-btn6.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 6!");
-		item = "6";
-		tg.MainButton.show();
-	}
-});
+  productCounts[productId] = count;
 
 
-Telegram.WebApp.onEvent("mainButtonClicked", function(){
-	tg.sendData(item);
-});
+  quantityElement.textContent = count;
+  quantityElement.style.display = count > 0 ? 'inline' : 'none';
+
+  const addButton = document.getElementById(`btn${productId}`);
+  const decrementButton = document.getElementById(`decrement${productId}`);
+  const imgg = document.getElementById(`img${productId}`);
 
 
-let usercard = document.getElementById("usercard");
+  if (count > 0) {
+    addButton.textContent = '+';
+    //decrementButton.style.display = 'inline';
+    addButton.classList.add('mini');
+    decrementButton.classList.add('show');
+    imgg.classList.add('rot');
+    
+  } else {
+    addButton.textContent = 'купить';
+    addButton.classList.remove('mini');
+    decrementButton.classList.remove('show');
 
-let p = document.createElement("p");
+  }
+}
 
-p.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}`;
 
-usercard.appendChild(p);
+function sendOrderToTelegram() {
+  const orderData = []; // Массив для хранения данных о заказе
+
+  // Собираем данные о заказе из productCounts
+  for (const productId in productCounts) {
+    if (productCounts[productId] > 0) {
+      orderData.push({
+        'id': productId, // ID товара
+        'quantity': productCounts[productId] // Количество
+      });
+    }
+  }
+
+  // Проверка, есть ли данные в заказе
+  if (orderData.length > 0) {
+    // Используйте Telegram.WebApp.onEvent для отправки данных
+    Telegram.WebApp.onEvent("mainButtonClicked", function() {
+      Telegram.WebApp.sendData(orderData);
+    });
+    Telegram.WebApp.sendData(orderData); // Отправляем данные
+  }
+}
+
+function updateSendButtonState() {
+  // Проверяем, есть ли товары в заказе
+  if (Object.values(productCounts).some(count => count > 0)) {
+    // Включаем главную кнопку Telegram Web App
+    Telegram.WebApp.MainButton.setParams({
+      text: "Отправить заказ",
+      show: true,
+      type: 'primary'
+    });
+  } else {
+    // Отключаем главную кнопку Telegram Web App
+    Telegram.WebApp.MainButton.setParams({
+      show: false
+    });
+  }
+}
+
+Telegram.WebApp.MainButton.onEvent('click', sendOrderToTelegram); 
+
+// Инициализация состояния кнопки при загрузке страницы
+updateSendButtonState(); 
