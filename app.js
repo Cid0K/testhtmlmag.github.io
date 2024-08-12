a2dbf5721ec007a03fddc89ef855217718c9850c
Cid0K <cidkonage@gmail.com>
2024-08-13 01:54:06 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,36 @@ tg.MainButton.textColor = '#FFFFFF';
 tg.MainButton.color = '#2cab37';
 tg.MainButton.show();
 
+const data = {
+  products: []
+};
+
+const tt = document.getElementById("data"); 
+
+function updateData() {
+  data.products = []; // Очищаем массив products перед сбором новых данных
+
+  for (const productId in productCounts) {
+    if (productCounts[productId] > 0) {
+      data.products.push({
+        id: productId,
+        quantity: productCounts[productId]
+      });
+    }
+  }
+
+  // Вывод данных в текст
+  tt.textContent = JSON.stringify(data, null, 2);
+
+  if (data.products.length > 0) {
+    tg.MainButton.show();
+    mb.textContent ='показана'
+  } else {
+    tg.MainButton.hide();
+    mb.textContent ='скрыта'
+}
+}
+
 function updateCount(productId, decrement = false) {
   const quantityElement = document.getElementById(`quantity${productId}`);
   let count = productCounts[productId] || 0;
@@ -24,55 +54,26 @@ function updateCount(productId, decrement = false) {
 
   const addButton = document.getElementById(`btn${productId}`);
   const decrementButton = document.getElementById(`decrement${productId}`);
-
+  
   if (count > 0) {
     addButton.textContent = '+';
     addButton.classList.add('mini');
     decrementButton.classList.add('show');
+    tt.textContent = JSON.stringify(data, null, 2);
+
   } else {
     addButton.textContent = 'купить';
     addButton.classList.remove('mini');
     decrementButton.classList.remove('show');
+    tt.textContent =''
   }
-
-  updateSendButtonState(); 
+  updateData();
 }
 
 
-function sendOrderToTelegram() {
-  const orderData = [];
 
-  for (const productId in productCounts) {
-    if (productCounts[productId] > 0) {
-      orderData.push({
-        'id': productId,
-        'quantity': productCounts[productId]
-      });
-    }
-  }
 
-  if (orderData.length > 0) {
-    Telegram.WebApp.onEvent("mainButtonClicked", function() {
-      tg.sendData(orderData);
-    });
-  
-  }
-}
-
-function updateSendButtonState() {
-  if (Object.values(productCounts).some(count => count > 0)) {
-    tg.MainButton.setParams({
-      text: "Отправить заказ",
-      show: true,
-      type: 'primary'
-    });
-  } else {
-    tg.MainButton.setParams({
-      show: false
-    });
-  }
-}
-
-Telegram.WebApp.MainButton.onEvent('click', sendOrderToTelegram); 
-
-updateSendButtonState(); 
+  // Отправляем данные в Telegram
+  Telegram.WebApp.onEvent("mainButtonClicked", function(){
+    tg.sendData(data);
+  });
