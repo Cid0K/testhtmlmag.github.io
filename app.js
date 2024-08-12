af848c5e9afd9dbc6d8c19e5d939fb2412e21f1a
Cid0K <cidkonage@gmail.com>
2024-08-12 14:37:29 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ tg.MainButton.textColor = '#FFFFFF';
 tg.MainButton.color = '#2cab37';
 
 function updateCount(productId, decrement = false) {
-  const quantityElement = document.getElementById(`quantity${productId}`);
+  const quantityElement = document.getElementById(quantity${productId});
   let count = productCounts[productId] || 0;
 
   if (decrement && count > 0) {
@@ -18,66 +18,57 @@ function updateCount(productId, decrement = false) {
 
   productCounts[productId] = count;
 
-
   quantityElement.textContent = count;
   quantityElement.style.display = count > 0 ? 'inline' : 'none';
 
-  const addButton = document.getElementById(`btn${productId}`);
-  const decrementButton = document.getElementById(`decrement${productId}`);
-  const imgg = document.getElementById(`img${productId}`);
-
+  const addButton = document.getElementById(btn${productId});
+  const decrementButton = document.getElementById(decrement${productId});
+  const imgg = document.getElementById(img${productId});
 
   if (count > 0) {
     addButton.textContent = '+';
-    //decrementButton.style.display = 'inline';
     addButton.classList.add('mini');
     decrementButton.classList.add('show');
-    imgg.classList.add('rot');
-    
+    imgg.classList.add('rot'); 
   } else {
     addButton.textContent = 'купить';
     addButton.classList.remove('mini');
     decrementButton.classList.remove('show');
-
   }
+
+  updateSendButtonState(); // Обновляем состояние кнопки после изменения количества
 }
 
 
 function sendOrderToTelegram() {
-  const orderData = []; // Массив для хранения данных о заказе
+  const orderData = []; 
 
-  // Собираем данные о заказе из productCounts
   for (const productId in productCounts) {
     if (productCounts[productId] > 0) {
       orderData.push({
-        'id': productId, // ID товара
-        'quantity': productCounts[productId] // Количество
+        'id': productId, 
+        'quantity': productCounts[productId] 
       });
     }
   }
 
-  // Проверка, есть ли данные в заказе
   if (orderData.length > 0) {
-    // Используйте Telegram.WebApp.onEvent для отправки данных
     Telegram.WebApp.onEvent("mainButtonClicked", function() {
       Telegram.WebApp.sendData(orderData);
     });
-    Telegram.WebApp.sendData(orderData); // Отправляем данные
+    Telegram.WebApp.sendData(orderData); 
   }
 }
 
 function updateSendButtonState() {
-  // Проверяем, есть ли товары в заказе
   if (Object.values(productCounts).some(count => count > 0)) {
-    // Включаем главную кнопку Telegram Web App
-      tg.WebApp.MainButton.setParams({
+    tg.WebApp.MainButton.setParams({
       text: "Отправить заказ",
       show: true,
       type: 'primary'
     });
   } else {
-    // Отключаем главную кнопку Telegram Web App
-      tg.WebApp.MainButton.setParams({
+    tg.WebApp.MainButton.setParams({
       show: false
     });
   }
@@ -86,4 +77,4 @@ function updateSendButtonState() {
 Telegram.WebApp.MainButton.onEvent('click', sendOrderToTelegram); 
 
 // Инициализация состояния кнопки при загрузке страницы
-updateSendButtonState(); 
+updateSendButtonState();
