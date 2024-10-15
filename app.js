2c452d1f853f9664c0fbe70c599ca553c026705f
Cid0K <cidkonage@gmail.com>
2024-10-15 19:23:52 +0300

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const data = {
   products: []
 };
 
-const tt = document.getElementById("data"); 
+//const tt = document.getElementById("data"); 
 
 function updateData() {
   console.log('updateData() called');
@@ -25,9 +25,20 @@ function updateData() {
       });
     }
   }
-}
 
-  tt.textContent = JSON.stringify(data, null, 2);
+  // Вывод данных в текст
+  //tt.textContent = JSON.stringify(data, null, 2);
+
+  if (data.products.length > 0) {
+    tg.MainButton.show();
+    //mb.textContent ='показана'
+    tg.MainButton.setText("Купить");
+  
+  } else {
+    tg.MainButton.hide();
+    //mb.textContent ='скрыта'
+}
+}
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
   tg.sendData(JSON.stringify(data));
